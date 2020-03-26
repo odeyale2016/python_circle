93714b921fe45b3b099e510b3abfd1365396f55a
odeyale kehinde <odeyale_kehinde@yahoo.com>
2020-03-26 12:46:07 +0100

    python task

---
+++ area_circle.py
@@ -0,0 +1,9 @@
+# Python Program to find Area Of Circle using Radius
+
+PI = 3.14
+radius = float(input(' Please Enter the radius of a circle: '))
+area = PI * radius * radius
+ 
+
+print(" Area Of a Circle = %.2f" %area)
+ 
